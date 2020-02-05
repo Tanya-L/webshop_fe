c72513342ddedd010443a015351ac19bd255da41
Tanya Ly <tanya.lytovchenko@gmail.com>
2020-02-05 15:24:54 +0100

    small fixed styles

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -21,7 +21,7 @@ export class ProductDetailComponent implements OnInit {
   }
 
   addToCart(product) {
-    // window.alert('Your product has been added to the cart!');
+    window.alert('Your product has been added to the cart!');
     this._service.addToCart(product);
   }
 
